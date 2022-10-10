34db9a52cfd5f9b6b2486fc0066445ed25889909
Marsel <marchello.marsel@gmail.com>
2022-10-10 16:00:58 +0600

    реализован сервис для запросов k API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const marvelService = new MarvelService();
+
+marvelService.getAllCaracters().then((res) =>
+	res.data.results.forEach((item) => {
+		console.log(item.name);
+	})
 );
 
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
