68f70cbbe4a945aa36eb672aa05ed69862f2465c
Marsel <marchello.marsel@gmail.com>
2022-10-11 00:34:14 +0600

    реализован компонент CharList, в RandomChar добавленно корректное отображения картинки

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,7 +13,9 @@ class MarvelService {
 	};
 
 	getAllCharacters = async () => {
-		const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+		const res = await this.getResource(
+			`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+		);
 		return res.data.results.map(this._transformCharacter);
 	};
 
@@ -24,6 +26,7 @@ class MarvelService {
 
 	_transformCharacter = (char) => {
 		return {
+			id: char.id,
 			name: char.name,
 			description: char.description
 				? `${char.description.slice(0, 210)}...`
