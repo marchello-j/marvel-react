ebadc0ca1c2729066e5c4250154d23af86ef1dfd
Marsel <marchello.marsel@gmail.com>
2022-10-11 21:55:48 +0600

    добавлена динамическая подгрузка персонажей

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	_apiKey = 'apikey=cba949ffd82da5ecd5ad14d1b764d3db';
+	_baseOffset = 210;
+
 
 	getResource = async (url) => {
 		let res = await fetch(url);
@@ -12,9 +14,9 @@ class MarvelService {
 		return await res.json();
 	};
 
-	getAllCharacters = async () => {
+	getAllCharacters = async (offset = this._baseOffset) => {
 		const res = await this.getResource(
-			`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+			`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
 		);
 		return res.data.results.map(this._transformCharacter);
 	};
