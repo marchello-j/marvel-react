5a6e15850d1c0715a8a26d588a493897a726caa0
Marsel <marchello.marsel@gmail.com>
2022-11-04 18:57:11 +0300

    создана страница comicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,33 +4,37 @@ import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
+import AppBanner from '../appBanner/AppBanner';
 
 import decoration from '../../resources/img/vision.png';
+import ComicsList from '../comicsList/ComicsList';
 const App = () => {
-	const [ selectedChar, setChar] = useState(null);
-	
+	const [selectedChar, setChar] = useState(null);
+
 	const onCharSelected = (id) => {
 		setChar(id);
 	};
-		return (
-			<div className='app'>
-				<AppHeader />
-				<main>
+	return (
+		<div className='app'>
+			<AppHeader />
+			<main>
+				{/* <ErrorBoundary>
+					<RandomChar />
+				</ErrorBoundary>
+				<div className='char__content'>
 					<ErrorBoundary>
-						<RandomChar />
+						<CharList onCharSelected={onCharSelected} />
 					</ErrorBoundary>
-					<div className='char__content'>
-						<ErrorBoundary>
-							<CharList onCharSelected={onCharSelected} />
-						</ErrorBoundary>
-						<ErrorBoundary>
-							<CharInfo charId={selectedChar} />
-						</ErrorBoundary>
-					</div>
-					<img className='bg-decoration' src={decoration} alt='vision' />
-				</main>
-			</div>
-		);
-}
+					<ErrorBoundary>
+						<CharInfo charId={selectedChar} />
+					</ErrorBoundary>
+				</div>
+				<img className='bg-decoration' src={decoration} alt='vision' /> */}
+				<AppBanner />
+				<ComicsList />
+			</main>
+		</div>
+	);
+};
 
 export default App;
